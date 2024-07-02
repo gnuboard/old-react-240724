200d7035966e52f2e740e2f6e625945f0615077d
dungdang39 <sth0701@icloud.com>
2024-07-02 15:08:52 +0900

    [feat] 더미데이터 재분류

---
+++ src/app/components/mobileMenu.js
@@ -0,0 +1,50 @@
+
+import React from 'react';
+import { Box, IconButton, Drawer, Typography, List, ListItem, ListItemText } from '@mui/material';
+import Menu from '@mui/icons-material/Menu';
+
+const MobileMenu = ({ drawerOpen, toggleDrawer, categories }) => {
+    return (
+        <>
+   <Box sx={{ display: { xs: 'block', md: 'none' }, position: 'fixed', bottom: 10, left: 10, width: '50px', height: '50px', backgroundColor: '#007aff', boxShadow: '0 -2px 5px rgba(0,0,0,0.1)', borderRadius: 1, zIndex: 1300 }}>
+        <IconButton onClick={toggleDrawer(true)} sx={{ width: '100%', color: '#fff' }}>
+          <Menu sx={{ fontSize: 30 }} />
+        </IconButton>
+      </Box>
+      <Drawer anchor="bottom" open={drawerOpen} onClose={toggleDrawer(false)}>
+  <Box sx={{ width: 'auto', p: 2 }}>
+    <Typography variant="h6" sx={{ mb: 2 }}>
+      Menu
+    </Typography>
+    <List>
+      {Object.keys(categories).map((mainCategory) => (
+        <Box key={mainCategory} mb={1}>
+          <Typography variant="p" sx={{ display: 'block', fontSize: '14px', backgroundColor: '#eee', color: '#555', width: '100%', fontWeight: 'bold', borderRadius: '4px', mb: 1, px: 1, py: 0.4 }}>
+            {mainCategory}
+          </Typography>
+          {categories[mainCategory].map((subCategory, index) => (
+            <ListItem button component="a" key={index} href={`/category/${index}`} sx={{ color: '#777', mb: 1, px: 1, py: 0.3 }}>
+              <ListItemText
+                primary={subCategory}
+                primaryTypographyProps={{
+                  style: {
+                    fontSize: '12px',
+                    color: '#000',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                  }
+                }}
+              />
+            </ListItem>
+          ))}
+        </Box>
+      ))}
+    </List>
+  </Box>
+</Drawer>
+</>
+ );
+};
+
+export default MobileMenu;
---
+++ src/app/data/dummyData.js
@@ -0,0 +1,73 @@
+const categories = {
+    "AIIZ Member": [
+      "AI최신정보",
+      "AI영상뉴스",
+      "AI이벤트",
+      "AI 갤러리",
+    ],
+    "Community": [
+      "자유게시판",
+      "OPENAI GPT",
+      "AI 질문/답변",
+    ],
+    "Tool Ai Service": [
+      "베스트 TOP100",
+      "디자인/이미지",
+      "영상/제작/편집",
+      "음악/음성/자막",
+      "컨텐츠/생산성",
+      "WEB/APP/개발",
+      "업무용/효율성",
+      "교육용/스터디",
+      "기타 AI 서비스"
+    ]
+  };
+
+  const items = [
+    { id: 1, title: 'Meta, 최신단 멀티모달 모델인 Chameleon 출시' },
+    { id: 2, title: 'Google I/O 2024: 구글이 발표한 100가지' },
+    { id: 3, title: 'Apple, 새로운 AI 기능이 포함된 Mac 및 iPad용 Logic Pro 앱...' },
+    { id: 4, title: 'OpenAI, ChatGPT 무료 사용자에게 유료 기능 개방' },
+    { id: 5, title: 'OpenAI, GPT-4 언어 모델 업그레이드 버전 ‘GPT-4o’ 발표' },
+  ];
+  
+  const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+      time: '24.01.26',
+      views: '158',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+      time: '24.01.26',
+      views: '158',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+      time: '24.01.26',
+      views: '158',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      time: '24.01.26',
+      views: '158',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+      time: '24.01.26',
+      views: '158',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+      time: '24.01.26',
+      views: '158',
+    }
+  ];
+
+  export { categories, items, itemData };
